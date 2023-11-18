f3d4ffbc28d785cc474e67036cecb31a4a48c668
florez <florez@ethz.ch>
2023-11-18 15:51:49 +0100

    now pls

---
+++ public/assets/js/cd2a9439.71e2ac12.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkdocs=self.webpackChunkdocs||[]).push([[1239],{6170:o=>{o.exports=JSON.parse('{"permalink":"/bamboost-docs/blog","page":1,"postsPerPage":10,"totalPages":1,"totalCount":4,"blogDescription":"Blog","blogTitle":"Blog"}')}}]);
