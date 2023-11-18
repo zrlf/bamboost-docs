92b94a3500c05cc48195149cf561af7069801846
florez <florez@ethz.ch>
2023-11-18 15:42:32 +0100

    test update do.config 2

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -11,7 +11,7 @@ const config: Config = {
   url: "https://zrlf.gitlab.io/bamboost-docs",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: "/",
+  baseUrl: "/bamboost-docs/",
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
