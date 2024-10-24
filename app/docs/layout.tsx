e4a21538b5a3e4db38b8a476b8f7d1fd3c7e1484
florez <florez@ethz.ch>
2024-10-24 18:47:52 +0200

    fix: add math plugin (may be unneccessary)

--- app/docs/layout.tsx
+++ app/docs/layout.tsx
@@ -2,6 +2,7 @@ import { DocsLayout } from "fumadocs-ui/layouts/docs";
 import type { ReactNode } from "react";
 import { baseOptions } from "@/app/layout.config";
 import { docSource } from "@/lib/source";
+import "katex/dist/katex.css";
 
 export default function Layout({ children }: { children: ReactNode }) {
   return (
--- source.config.ts
+++ source.config.ts
@@ -1,17 +1,18 @@
-import { defineDocs, defineConfig, GlobalConfig } from 'fumadocs-mdx/config';
+import { defineDocs, defineConfig, GlobalConfig } from "fumadocs-mdx/config";
 import { rehypeCode } from "fumadocs-core/mdx-plugins";
 import { shikiTheme } from "./constants";
+import remarkMath from "remark-math";
+import rehypeKatex from "rehype-katex";
 
 export const { docs, meta } = defineDocs({
-  dir: 'content/docs',
+  dir: "content/docs",
 });
 
 const config: GlobalConfig = {
   mdxOptions: {
-    rehypePlugins: [[rehypeCode, { themes: {...shikiTheme} }]],
+    rehypePlugins: [rehypeKatex, [rehypeCode, { themes: { ...shikiTheme } }]],
+    remarkPlugins: [remarkMath],
   },
 };
 
 export default defineConfig(config);
-
-
