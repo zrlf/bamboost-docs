cd8aaca702682d499b9e721adb6df5645023eb1e
florez <florez@ethz.ch>
2025-05-11 03:12:54 +0200

    feat: switch search to orama cloud

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,30 +1,18 @@
 import Link from "fumadocs-core/link";
 import { Banner } from "fumadocs-ui/components/banner";
-import { RootProvider } from "fumadocs-ui/provider";
 import type { ReactNode } from "react";
 import { Body } from "./layout.client";
 import { GeistSans } from "geist/font/sans";
-import 'katex/dist/katex.css';
+import "katex/dist/katex.css";
 import "./global.css";
+import { RootProvider } from "fumadocs-ui/provider";
+import { CustomSearchDialog } from "@/components/searchOrama";
 
 export default function Layout({ children }: { children: ReactNode }) {
   return (
     <html lang="en" className={GeistSans.className} suppressHydrationWarning>
       <Body>
-        <RootProvider
-          search={{
-            options: {
-              api: "/api/search",
-              allowClear: true,
-              type: "static",
-              tags: [
-                { name: "API", value: "api-bamboost" },
-                { name: "Documentation", value: "docs" },
-                // { name: "TUI", value: "api-bamboostcli" },
-              ],
-            },
-          }}
-        >
+        <RootProvider search={{ SearchDialog: CustomSearchDialog }}>
           <Banner variant="rainbow" id="dev-version-banner">
             <div>
               You are viewing the documentation for the <b>dev</b> branch of
