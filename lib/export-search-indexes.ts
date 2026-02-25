2c25df587e6d47ab9abf0513fd03222863632014
florez <florez@ethz.ch>
2026-02-25 17:59:53 +0100

    chore: update search with breadcrumbs

--- lib/export-search-indexes.ts
+++ lib/export-search-indexes.ts
@@ -1,10 +1,16 @@
 import { docSource } from "@/lib/source";
 import { autodocSources } from "@/lib/autodocSource";
+import { getBreadcrumbItems } from "fumadocs-core/breadcrumb";
 
 export async function exportSearchIndexes() {
   const results: any[] = [];
 
   for (const page of docSource.getPages()) {
+    const items = getBreadcrumbItems(page.url, docSource.getPageTree(), {
+      includePage: false,
+      includeRoot: true,
+    });
+
     results.push({
       id: page.url,
       structuredData: page.data.structuredData,
@@ -12,11 +18,19 @@ export async function exportSearchIndexes() {
       title: page.data.title,
       description: page.data.description,
       tag: "docs",
+      breadcrumbs: items.flatMap<string>((item, i) =>
+        i > 0 && typeof item.name === "string" ? item.name : [],
+      ),
     });
   }
 
   for (const [sourceKey, source] of Object.entries(autodocSources)) {
     for (const page of source.getPages()) {
+      const items = getBreadcrumbItems(page.url, source.getPageTree(), {
+        includePage: false,
+        includeRoot: true,
+      });
+
       results.push({
         id: page.url,
         structuredData: page.data.structuredData,
@@ -24,6 +38,9 @@ export async function exportSearchIndexes() {
         title: page.data.title,
         description: page.data.description,
         tag: `api-${sourceKey}`,
+        breadcrumbs: items.flatMap<string>((item, i) =>
+          i > 0 && typeof item.name === "string" ? item.name : [],
+        ),
       });
     }
   }
